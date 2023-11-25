489f4ddfd2007f88e5b5844e9933014dc3a39aae
akda007 <akdagmr007s@gmail.com>
2023-11-25 18:59:19 -0300

    Finished the simulator logic

--- main.c
+++ main.c
@@ -4,77 +4,72 @@
 
 #include "mips.h"
 
-typedef enum {
-    I, R, J
-} InstructionType;
-
 #define compare(i, c, func) if (strcmp(i, c) == 0) (func)(cpu) 
-
 void parse_instruction(Cpu_T *cpu, char * instruction) {
-    InstructionType type;    
+    printf("Instruction memory: busca de operacao\n\n");
 
-    compare(instruction, "add", add);
-    compare(instruction, "addu", addu);
-    compare(instruction, "and", and);
-    compare(instruction, "div", div);
-    compare(instruction, "divu", divu);
-    compare(instruction, "jalr", jalr);
-    compare(instruction, "jr", jr);
-    compare(instruction, "mfhi", mfhi);
-    compare(instruction, "mthi", mthi);
-    compare(instruction, "mflo", mflo);
-    compare(instruction, "mtlo", mtlo);
-    compare(instruction, "mfc0", mfc0);
-    compare(instruction, "mult", mult);
+    printf("Incrementa PC.\n");
+    cpu->registers[PC] += 4;
+
+    //Macros para escolher uma operaçao
+    printf("Decodifica instrucao:\n\n");
+    
+    compare(instruction,   "add",   add);
+    compare(instruction,  "move",  move);
+    compare(instruction,  "addu",  addu);
+    compare(instruction,   "and",   and);
+    compare(instruction,   "div",   div);
+    compare(instruction,  "divu",  divu);
+    compare(instruction,    "jr",    jr);
+    compare(instruction,  "mfhi",  mfhi);
+    compare(instruction,  "mflo",  mflo);
+    compare(instruction,  "mult",  mult);
     compare(instruction, "multu", multu);
-    compare(instruction, "nor", nor);
-    compare(instruction, "xor", xor);
-    compare(instruction, "or", or);
-    compare(instruction, "slt", slt);
-    compare(instruction, "sltu", sltu);
-    compare(instruction, "sll", sll);
-    compare(instruction, "srl", srl);
-    compare(instruction, "sra", sra);
-    compare(instruction, "sub", sub);
-    compare(instruction, "subu", subu);
-    compare(instruction, "j", j);
-    compare(instruction, "jal", jal);
-    compare(instruction, "addi", addi);
+    compare(instruction,   "nor",   nor);
+    compare(instruction,   "xor",   xor);
+    compare(instruction,    "or",    or);
+    compare(instruction,   "sll",   sll);
+    compare(instruction,   "srl",   srl);
+    compare(instruction,   "sra",   sra);
+    compare(instruction,   "sub",   sub);
+    compare(instruction,  "subu",  subu);
+    compare(instruction,     "j",     j);
+    compare(instruction,   "jal",   jal);
+    compare(instruction,  "addi",  addi);
     compare(instruction, "addiu", addiu);
-    compare(instruction, "andi", andi);
-    compare(instruction, "beq", beq);
-    compare(instruction, "blez", blez);
-    compare(instruction, "bne", bne);
-    compare(instruction, "bgtz", bgtz);
-    compare(instruction, "lb", lb);
-    compare(instruction, "lbu", lbu);
-    compare(instruction, "lhu", lhu);
-    compare(instruction, "lui", lui);
-    compare(instruction, "lw", lw);
-    compare(instruction, "ori", ori);
-    compare(instruction, "sb", sb);
-    compare(instruction, "sh", sh);
-    compare(instruction, "slti", slti);
-    compare(instruction, "sltiu", sltiu);
-    compare(instruction, "sw", sw);
-
+    compare(instruction,  "andi",  andi);
+    compare(instruction,   "beq",   beq);
+    compare(instruction,   "bne",   bne);
+    compare(instruction,    "lb",    lb);
+    compare(instruction,    "lw",    lw);
+    compare(instruction,   "ori",   ori);
+    compare(instruction,    "sb",    sb);
+    compare(instruction,    "sw",    sw);
+    compare(instruction,    "li",    li);
 }
 
 int main() {
     puts("Mips simulator.");
 
     Cpu_T cpu;
+    cpu.HI = 0;
+    cpu.LO = 0;
+
+    //Initialize registers to 0
+    for (int i = 0; i < 32; i++) {
+        cpu.registers[i] = 0;
+    }
 
     char op[6];
-    uint8_t rs, rt, rd;
 
     for(;;) {
         printf("Digite uma operacao:\n> ");
         scanf("%5s", op);
 
-        parse_instruction(op);
+        printf("PC: 0x%x\n", cpu.registers[PC]);
+        parse_instruction(&cpu, op);
 
-        cpu.memory[PC]++;        
+        printf("PC: 0x%x\n", cpu.registers[PC]);
     }
     return 0;
 }
--- mips.h
+++ mips.h
@@ -1,34 +1,121 @@
 
+#include <stdio.h>
+#include <stdint.h>
+
 #define PC 31
 #define SP 30
+#define RA 29
 
+//Macro para facilitar o acesso aos registradores
 #define REG(cpu, x) cpu->registers[x]
+#define RAM(cpu, x) cpu->memory[x]
+
+#define ABS(x) x < 0 ? (-x) : x
 
 typedef struct Cpu {
-    float registers[32];
-    float memory [8192];
+    int32_t registers[32];
+    uint8_t memory [8192];
+    int64_t HI, LO;
 } Cpu_T;
+
+void get_dataImediate(int *rd, int *rs, int *imediate) {
+    printf("Rd: ");
+    scanf("%d", rd);
+    printf("Rs: ");
+    scanf("%d", rs);
+    printf("i: ");
+    scanf("%d", imediate);
+}
+
+void get_condJ(int *rs, int *rt, int *label_addres) {
+    printf("Rs: ");
+    scanf("%d", rs);
+    printf("Rt: ");
+    scanf("%d", rt);
+    printf("Label_Addres: ");
+    scanf("%d", label_addres);
+
+    printf("Le registradores...\n\n");
+    printf("ULA: Operacao da ula");
+}
 
 
 void get_dataRI(int *rd, int *rs, int *rt) {
     printf("Rd: ");
-    scanf("%d", rd)
+    scanf("%d", rd);
     printf("Rs: ");
-    scanf("%d", rs)
+    scanf("%d", rs);
     printf("Rt: ");
-    scanf("%d", rt)
+    scanf("%d", rt);
+
+    printf("Le registradores...\n\n");
+    printf("ULA: Operacao da ula");
 }
 
 void get_dataLoad(int *rs, int *rt) {
     printf("Rs: ");
-    scanf("%d", rs)
-    printf("Rs: ");
-    scanf("%d", rt)
+    scanf("%d", rs);
+    printf("Rt: ");
+    scanf("%d", rt);
+
+    printf("Le registradores...\n\n");
+    printf("ULA: Operacao da ula");
+}
+
+void get_Reg(int *reg, int * ram) {
+    printf("Reg: ");
+    scanf("%d", reg);
+    printf("Ram: ");
+    scanf("%d", ram);
+
+    printf("Le registradores...\n\n");
+    printf("ULA: Operacao da ula");
 }
 
 void get_dataJ(int * data) {
     printf("Addres: ");
     scanf("%d", data);
+
+    printf("Le registradores...\n\n");
+    printf("ULA: Operacao da ula");
+}
+
+void print_R(Cpu_T * cpu, int rd, int rs, int rt) {
+    printf("Escreve registradores...\n\n");
+
+
+    printf("R%d: 0x%X | dec: (%d)\n", rd, REG(cpu, rd), REG(cpu, rd));
+    printf("R%d: 0x%X | dec: (%d)\n", rs, REG(cpu, rs), REG(cpu, rs));
+    printf("R%d: 0x%X | dec: (%d)\n", rt, REG(cpu, rt), REG(cpu, rt));
+}
+
+void print_RI(Cpu_T * cpu, int rd, int rs) {
+    printf("Escreve registradores...\n\n");
+
+    
+    printf("R%d: 0x%X | dec: (%d)\n", rd, REG(cpu, rd), REG(cpu, rd));
+    printf("R%d: 0x%X | dec: (%d)\n", rs, REG(cpu, rs), REG(cpu, rs));
+}
+
+void print_Reg(Cpu_T *cpu, int r) {
+    printf("Escreve registradores...\n\n");
+
+    
+    printf("R%d: 0x%X | dec: (%d)\n", r, REG(cpu, r), REG(cpu, r));
+}
+
+void printMem(Cpu_T *cpu, int ram, int offset) {
+    printf("Operacoes na memoria(read/write)\n");
+    printf("Escreve registradores...\n\n");
+
+    
+    printf("Hex_RAM(0x%X --> 0x%X): ", ram, ram + offset - 1);
+
+    for (int i = 0; i < offset; i++) {
+        printf("%X ", RAM(cpu, ram + i));
+    }
+
+    printf("\n");
 }
 
 void add(Cpu_T *cpu) {
@@ -36,349 +123,358 @@ void add(Cpu_T *cpu) {
     get_dataRI(&rd, &rs, &rt);
 
     REG(cpu, rd) = REG(cpu, rs) + REG(cpu, rt);
+
+    print_R(cpu, rd, rs, rt);
 }
 
-void addu() {
+void li(Cpu_T *cpu) {
+    int rd, i;
+    printf("Rd: ");
+    scanf("%d", &rd);
+    printf("i: ");
+    scanf("%d", &i);
+
+    REG(cpu, rd) = i;
+    print_Reg(cpu, rd);
+}
+
+void addu(Cpu_T *cpu) {
     int rd, rs, rt;
     get_dataRI(&rd, &rs, &rt);
 
+    REG(cpu, rs) = ABS(REG(cpu,rs));
+    REG(cpu, rt) = ABS(REG(cpu,rt));
+
+    REG(cpu, rd) = REG(cpu, rs) + REG(cpu, rt);
+    print_R(cpu, rd, rs, rt);
+}
+
+void and(Cpu_T *cpu) {
+    int rd, rs, rt;
+    get_dataRI(&rd, &rs, &rt);
+
+    REG(cpu, rd) = REG(cpu, rs) & REG(cpu, rt);
+    print_R(cpu, rd, rs, rt);
+}
+
+void div(Cpu_T *cpu) {
+    int rd, rs, rt;
+    get_dataRI(&rd, &rs, &rt);
+
+    REG(cpu, rd) = (int)REG(cpu, rs) / (int)REG(cpu, rt);
+    print_R(cpu, rd, rs, rt);
+
+}
+
+void divu(Cpu_T *cpu) {
+    int rd, rs, rt;
+    get_dataRI(&rd, &rs, &rt);
+
+    REG(cpu, rs) = ABS(REG(cpu,rs));
+    REG(cpu, rt) = ABS(REG(cpu,rt));
+
+    REG(cpu, rd) = REG(cpu, rs) / REG(cpu, rt);
+    print_R(cpu, rd, rs, rt);
+}
+
+
+void jr(Cpu_T *cpu) {
+    int rs;
+    get_dataJ(&rs);
+
+    REG(cpu, PC) = REG(cpu, rs);
+    //code
+}
+
+void mfhi(Cpu_T *cpu) {
+    int rd;
+    printf("Reg: ");
+    scanf("%d", &rd);
+    
+    REG(cpu, rd) = cpu->HI;
+}
+
+
+void mflo(Cpu_T *cpu) {
+    int rd;
+    printf("Reg: ");
+    scanf("%d", &rd);
+    
+    REG(cpu, rd) = cpu->LO;
+}
+
+void mult(Cpu_T *cpu) {
+    int rd, rs, rt;
+    get_dataRI(&rd, &rs, &rt);
+
+    REG(cpu, rd) = REG(cpu, rs) * REG(cpu, rt);
+    print_R(cpu, rd, rs, rt);
+}
+
+void multu(Cpu_T *cpu) {
+    int rd, rs, rt;
+    get_dataRI(&rd, &rs, &rt);
+
+    REG(cpu, rs) = ABS(REG(cpu,rs));
+    REG(cpu, rt) = ABS(REG(cpu,rt));
+
+    REG(cpu, rd) = REG(cpu, rs) * REG(cpu, rt);
+    print_R(cpu, rd, rs, rt);
+}
+
+void nor(Cpu_T *cpu) {
+    int rd, rs, rt;
+    get_dataRI(&rd, &rs, &rt);
+
+    REG(cpu, rd) = ~(REG(cpu, rs) | REG(cpu, rt));
+    print_R(cpu, rd, rs, rt);
+}
+
+void xor(Cpu_T *cpu) {
+    int rd, rs, rt;
+    get_dataRI(&rd, &rs, &rt);
+
+    REG(cpu, rd) = REG(cpu, rs) ^ REG(cpu, rt);
+    print_R(cpu, rd, rs, rt);
+
+}
+
+void or(Cpu_T *cpu) {
+    int rd, rs, rt;
+    get_dataRI(&rd, &rs, &rt);
+
+    //code
+    REG(cpu, rd) = REG(cpu, rs) | REG(cpu, rt);
+    print_R(cpu, rd, rs, rt);
+}
+
+
+void sll(Cpu_T *cpu) {
+    int rd, rs, i;
+    get_dataImediate(&rd, &rs, &i);
+
+    //code
+    REG(cpu, rd) = REG(cpu, rs) << i;
+    print_RI(cpu, rd, rs);
+}
+
+void srl(Cpu_T *cpu) {
+    int rd, rs, i;
+    get_dataImediate(&rd, &rs, &i);
+
+    //code
+    REG(cpu, rd) = REG(cpu, rs) >> i;
+    print_RI(cpu, rd, rs);
+}
+
+
+void sra(Cpu_T *cpu) {
+    int rd, rs, i;
+    get_dataImediate(&rd, &rs, &i);
+
+
+    REG(cpu, rd) = REG(cpu, rs) << i;
+    print_RI(cpu, rd, rs);
+}
+
+
+void sub(Cpu_T *cpu) {
+    int rd, rs, rt;
+    get_dataRI(&rd, &rs, &rt);
+
+    //code
+    REG(cpu, rd) = REG(cpu, rs) - REG(cpu, rt);
+    print_R(cpu, rd, rs, rt);
+}
+
+void subu(Cpu_T *cpu) {
+    int rd, rs, rt;
+    get_dataRI(&rd, &rs, &rt);
+
+    REG(cpu, rs) = ABS(REG(cpu,rs));
+    REG(cpu, rt) = ABS(REG(cpu,rt));
+
+    //code
+    REG(cpu, rd) = REG(cpu, rs) - REG(cpu, rt);
+    print_R(cpu, rd, rs, rt);
+}
+
+void j(Cpu_T *cpu) {
+    int addres;
+    get_dataJ(&addres);
+
+    //code
+    REG(cpu, PC) = addres;
+}
+
+void jal(Cpu_T *cpu) {
+    int addres;
+    get_dataJ(&addres);
+
+    //code
+    REG(cpu, RA) = REG(cpu, PC);
+    REG(cpu, PC) = addres;
+}
+
+void addi(Cpu_T *cpu) {
+    int rd, rs, imediate;
+    get_dataImediate(&rd, &rs, &imediate);
+
+    REG(cpu, rd) = REG(cpu, rs) + imediate;
+
+    //code
+    print_RI(cpu, rd, rs);
+}
+
+void addiu(Cpu_T *cpu) {
+    int rd, rs, imediate;
+    get_dataImediate(&rd, &rs, &imediate);
+
+    REG(cpu, rs) = ABS(REG(cpu,rs));
+    imediate = ABS(imediate);
     //code
     
+    REG(cpu, rd) = REG(cpu, rs) + imediate;
+    print_RI(cpu, rd, rs);
 }
 
-void and() {
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
+void andi(Cpu_T *cpu) {
+    int rd, rs, imediate;
+    get_dataImediate(&rd, &rs, &imediate);
+
+    REG(cpu, rd) = REG(cpu, rs) & imediate;
+    print_RI(cpu, rd, rs);
+}
+
+void beq(Cpu_T *cpu) {
+    int rs, rt, addres;
+
+    get_condJ(&rs, &rt, &addres);
 
     //code
-
+    if (REG(cpu, rs) == REG(cpu, rt)) {
+        REG(cpu, PC) = addres;
+    }
 }
 
-void div() {
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
+void bne(Cpu_T *cpu) {
+    int rs, rt, addres;
+
+    get_condJ(&rs, &rt, &addres);
 
     //code
-
+    if (REG(cpu, rs) != REG(cpu, rt)) {
+        REG(cpu, PC) = addres;
+    }
 }
 
-void divu() {
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
+void ble(Cpu_T *cpu) {
+    int rs, rt, addres;
+
+    get_condJ(&rs, &rt, &addres);
 
     //code
-
+    if (REG(cpu, rs) <= REG(cpu, rt)) {
+        REG(cpu, PC) = addres;
+    }
 }
+void blt(Cpu_T *cpu) {
+    int rs, rt, addres;
 
-void jalr() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
+    get_condJ(&rs, &rt, &addres);
 
     //code
+    if (REG(cpu, rs) < REG(cpu, rt)) {
+        REG(cpu, PC) = addres;
+    }
 }
 
-void jr() {
 
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
+void bgt(Cpu_T *cpu) {
+    int rs, rt, addres;
+
+    get_condJ(&rs, &rt, &addres);
 
     //code
+    if (REG(cpu, rs) > REG(cpu, rt)) {
+        REG(cpu, PC) = addres;
+    }
 }
 
-void mfhi() {
+void bge(Cpu_T *cpu) {
+    int rs, rt, addres;
 
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
+    get_condJ(&rs, &rt, &addres);
 
     //code
+    if (REG(cpu, rs) >= REG(cpu, rt)) {
+        REG(cpu, PC) = addres;
+    }
 }
 
-void mthi() {
+void lb(Cpu_T *cpu) {
+    int rd, ram;
+    get_Reg(&rd, &ram);
 
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
+
+    REG(cpu, rd) = RAM(cpu, ram);
+
+    printMem(cpu, ram, 1);
+    print_Reg(cpu, rd);
+}
+
+
+
+void lw(Cpu_T *cpu) {
+    int rd, ram;
+    get_Reg(&rd, &ram);
+
+    REG(cpu, rd) = (int32_t)RAM(cpu, ram+3);
+
+    REG(cpu, rd) = REG(cpu, rd) | ((int32_t)RAM(cpu, ram+2) << 8);
+    REG(cpu, rd) = REG(cpu, rd) | ((int32_t)RAM(cpu, ram+1) << 16);
+    REG(cpu, rd) = REG(cpu, rd) | ((int32_t)RAM(cpu, ram) << 24);
+    
+    printMem(cpu, ram, 4);
+    print_Reg(cpu, rd);
+}
+
+void ori(Cpu_T *cpu) {
+    int rd, rs, imediate;
+    get_dataImediate(&rd, &rs, &imediate);
 
     //code
+    REG(cpu, rd) = REG(cpu, rs) | imediate;
 }
 
-void mflo() {
+void sb(Cpu_T *cpu) {
+    int rs, ram;
+    get_Reg(&rs, &ram);
 
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
+    RAM(cpu, ram) = REG(cpu, rs) & 0xff;
+    printMem(cpu, ram, 1);
 }
 
-void mtlo() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void mfc0() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void mult() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void multu() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void nor() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void xor() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void or() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void slt() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void sltu() {
+void move(Cpu_T *cpu){
 
     int rd, rs, rt;
     get_dataRI(&rd, &rs, &rt);
 
-    //code
+    REG(cpu, rd) = REG(cpu, rs);
+    
+    print_R(cpu, rd, rs, rt);
 }
 
-void sll() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void srl() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void sra() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void sub() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void subu() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void j() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void jal() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void addi() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void addiu() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void andi() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void beq() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void blez() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void bne() {
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-
-}
-
-void bgtz() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void lb() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void lbu() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void lhu() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void lui() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void lw() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void ori() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void sb() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void sh() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void slti() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void sltiu() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
-}
-
-void sw() {
-
-    int rd, rs, rt;
-    get_dataRI(&rd, &rs, &rt);
-
-    //code
+void sw(Cpu_T *cpu) {
+    int rs, ram;
+    get_Reg(&rs, &ram);
+
+    
+    RAM(cpu, ram+3) =  REG(cpu, rs)          & 0xff;
+    RAM(cpu, ram+2) = (REG(cpu, rs) >> 8)    & 0xff;
+    RAM(cpu, ram+1) = (REG(cpu, rs) >> 16)   & 0xff;
+    RAM(cpu, ram)   = (REG(cpu, rs) >> 24)   & 0xff;
+
+    printMem(cpu, ram, 4);
 }
 
